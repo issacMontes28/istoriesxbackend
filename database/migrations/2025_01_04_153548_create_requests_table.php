7f39b7f5ac353c3d20ada5fe734d9fba9d313cea
issacMontes28 <issac@butchershop.co>
2025-01-05 13:23:13 +0000

    Created migrations for media, platform and requests, added personal access token tableand seeder for test user

---
+++ database/migrations/2025_01_04_153548_create_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('platform');
+            $table->string('url');
+            $table->enum('status', ['pending', 'completed', 'failed'])->default('pending');
+            $table->string('media_type'); // video, image, etc.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
---
+++ database/seeders/TestUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class TestUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => Hash::make('password'), // Use a hashed password
+        ]);
+    }
+}
