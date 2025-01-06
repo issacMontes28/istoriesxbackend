1555ff034916b57084fd0713aa104fee9be9d6f1
issacMontes28 <issac@butchershop.co>
2025-01-06 12:34:03 +0000

    First react configuration

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-    return <h1>Hello from React!</h1>;
+    return <h1>istoriesx</h1>;
 }
 
 ReactDOM.createRoot(document.getElementById('app')).render(<App />);
